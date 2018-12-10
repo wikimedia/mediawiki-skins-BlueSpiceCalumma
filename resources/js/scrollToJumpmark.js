0e7ee15f433f4848000ff605506920357ff6743a
dvogel <daniel.vogel@hallowelt.com>
2018-12-10 08:03:54 +0100

    Fix: Scroll to jumpmark
    Change-Id: I4aa6f3bd19c099d4a4c266367f125694bb56fa18

---
+++ resources/js/scrollToJumpmark.js
@@ -0,0 +1,46 @@
+( function( d, $, mw ) {
+	/* scroll to jumpmark on load */
+	var hash = window.location.hash;
+
+	if ( hash !== '' ) {
+		var jumpmarkId = hash.replace( '#', '' );
+
+		var jumpmark = d.getElementById( jumpmarkId );
+		var position = $( jumpmark ).position().top;
+
+		$( 'body, html').animate(
+			{
+				scrollTop: position
+			},
+			300
+		);
+	};
+
+	/* scroll to jumpmark on click (TOC,...) */
+	$( '#bodyContent a' ).on( 'click', function( e ){
+		var $anchor = $( this );
+		var hash = this.hash;
+
+		if( $anchor.hasClass( 'external' ) ) {
+			return;
+		}
+
+		if ( $anchor.attr( 'href' ).indexOf( '#' ) !== 0 ) {
+			return;
+		}
+
+		if ( hash !== '' ) {
+			var jumpmarkId = hash.replace( '#', '' );
+
+			var jumpmark = d.getElementById( jumpmarkId );
+			var position = $( jumpmark ).position().top;
+
+			$( 'body, html').animate(
+				{
+					scrollTop: position
+				},
+				800
+			);
+		}
+	});
+}) ( document, jQuery, mediaWiki);
