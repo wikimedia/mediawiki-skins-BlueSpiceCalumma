64646b9c03acacebbe01f9fffd0312d20d44fe1f
dvogel <daniel.vogel@hallowelt.com>
2019-04-02 11:01:32 +0200

    Change runtime for scrollToJumpmark code.
    Please cherry-pick to REL1_31
    
    Change-Id: Ief17af3913008f19cdf8b7d6baf8d47f7bb186c9

--- resources/js/scrollToJumpmark.js
+++ resources/js/scrollToJumpmark.js
@@ -1,49 +1,51 @@
 ( function( d, $, mw ) {
-	/* scroll to jumpmark on load */
-	var hash = window.location.hash;
-
-	if ( hash !== '' ) {
-		var jumpmarkId = hash.replace( '#', '' );
-
-		var jumpmark = d.getElementById( jumpmarkId );
-		if ( !jumpmark ) {
-			return;
-		}
-		var position = $( jumpmark ).position().top;
-
-		$( 'body, html').animate(
-			{
-				scrollTop: position
-			},
-			300
-		);
-	};
-
-	/* scroll to jumpmark on click (TOC,...) */
-	$( '#bodyContent a' ).on( 'click', function( e ){
-		var $anchor = $( this );
-		var hash = this.hash;
-
-		if( $anchor.hasClass( 'external' ) ) {
-			return;
-		}
-
-		if ( !$anchor.attr( 'href' ) || $anchor.attr( 'href' ).indexOf( '#' ) !== 0 ) {
-			return;
-		}
+	$( d ).ready( function(){
+		/* scroll to jumpmark on load */
+		var hash = window.location.hash;
 
 		if ( hash !== '' ) {
 			var jumpmarkId = hash.replace( '#', '' );
 
 			var jumpmark = d.getElementById( jumpmarkId );
+			if ( !jumpmark ) {
+				return;
+			}
 			var position = $( jumpmark ).position().top;
 
 			$( 'body, html').animate(
 				{
 					scrollTop: position
 				},
-				400
+				100
 			);
-		}
+		};
+
+		/* scroll to jumpmark on click (TOC,...) */
+		$( '#bodyContent' ).on( 'click', 'a', function( e ){
+			var $anchor = $( this );
+			var hash = this.hash;
+
+			if( $anchor.hasClass( 'external' ) ) {
+				return;
+			}
+
+			if ( !$anchor.attr( 'href' ) || $anchor.attr( 'href' ).indexOf( '#' ) !== 0 ) {
+				return;
+			}
+
+			if ( hash !== '' ) {
+				var jumpmarkId = hash.replace( '#', '' );
+
+				var jumpmark = d.getElementById( jumpmarkId );
+				var position = $( jumpmark ).position().top;
+
+				$( 'body, html').animate(
+					{
+						scrollTop: position
+					},
+					400
+				);
+			}
+		});
 	});
 }) ( document, jQuery, mediaWiki);
