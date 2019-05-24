d8221d35d5d677a2d224d9baf9f5a4a00749fbc7
dvogel <daniel.vogel@hallowelt.com>
2019-05-24 10:19:52 +0200

    scrollToJumpmark.js: Fix issue with not working tabs at Special:Preferences.
    Please cherry-pick to master.
    
    Change-Id: I400972fe4d8cb75c5b487dd4747381e15f90d619

--- resources/js/scrollToJumpmark.js
+++ resources/js/scrollToJumpmark.js
@@ -21,13 +21,17 @@
 			);
 		};
 
-		/* scroll to jumpmark on click (TOC, imagemap, ...) */
-		$( '#bodyContent' ).on( 'click', 'a, map area', function( e ) {
-			var hash = this.hash;
+		/* scroll to jumpmark on click (TOC, imagemap, ...).
+		 * accroding to syntax for jumpmarks at
+		 * https://www.mediawiki.org/wiki/Help:Links/en#Internal_links
+		 * there is no id and class used for jumpmarks
+		 * (see: "Link to an anchor on the same page")
+		 */
+		$( '#bodyContent' ).on( 'click',
+			'#mw-content-text a:not( [id] ):not( [class] ),	#mw-content-text map area',
+			function( e ) {
 
-			if( $( this ).hasClass( 'external' ) ) {
-				return;
-			}
+			var hash = this.hash;
 
 			if( this.pathname !== mw.util.getUrl() ) {
 				return;
