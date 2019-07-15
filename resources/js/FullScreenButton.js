66fac236d8008c0ca4037f0788df502bfe16de20
Dmitry Shevliakov <dsh@astarios.com>
2019-07-15 12:46:54 +0300

    Create new functionality for fullscreen mode
    Add closing tabs when pressing the button
    
    ERM 15030
    
    Change-Id: Icb3760bdd1bd6e75d77c3dfc9193fa87a9bd6b59

--- resources/js/FullScreenButton.js
+++ resources/js/FullScreenButton.js
@@ -7,7 +7,14 @@
 		if ( $( this ).hasClass( target ) ){
 			$( this ).removeClass( target );
 			$( 'body.mediawiki .wrapper .bs-content.container' ).removeClass( target );
+
 		} else{
+			if ( !( $( 'body' ).hasClass( 'navigation-main-collapse' ) ) ){
+				$( '.navigation-main .sidebar-toggle' ).trigger( 'click' );
+			}
+			if ( !( $( 'body' ).hasClass( 'sitetools-main-collapse' ) ) ){
+				$( '.sitetools-main .sidebar-toggle' ).trigger( 'click' );
+			}
 			$( 'body.mediawiki .wrapper .bs-content.container' ).addClass( target );
 			$( this ).addClass( target );
 		}
