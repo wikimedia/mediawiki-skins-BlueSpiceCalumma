99fd4759ac9a2b8e03e587705a26b9d1210f6e07
rvogel <vogel@hallowelt.biz>
2019-08-08 13:27:30 +0200

    Category section improvements
    * Category pills: reduce corner radius
    * Remove "Categories" panel from "qualitymanagement" panel in site tools
    
    Not solved here
    * Category icon: use outline icon --> We don't have such an icon at the moment
    
    ERM16074
    
    NEEDS CHERRY-PICK TO `master`!
    
    Change-Id: I214f57cd477d0a865b168e378e6d042a7068951f

--- src/Panel/QualityManagement.php
+++ src/Panel/QualityManagement.php
@@ -37,14 +37,7 @@ class QualityManagement extends PanelContainer implements IActiveStateProvider {
 	 * @return \BlueSpice\Calumma\IPanel[]
 	 */
 	protected function makePanels() {
-		$defaultPanelDefs = [
-			'categories' => [
-				'position' => 10,
-				'callback' => function ( $sktemplate ) {
-					return new Categories( $sktemplate );
-				}
-			]
-		];
+		$defaultPanelDefs = [];
 		$panelDefs = $this->skintemplate->get( SkinData::PAGE_DOCUMENTS_PANEL );
 
 		$combinedPanelDefs = array_merge_recursive(
