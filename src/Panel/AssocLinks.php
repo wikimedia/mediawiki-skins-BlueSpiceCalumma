214d82bfd8000eb98d24d30fd8e87ca27c690077
rvogel <vogel@hallowelt.biz>
2019-08-16 12:03:18 +0200

    Add "associated links" panel
    NEEDS CHERRY-PICK TO `master`!
    
    Change-Id: I79bdd73c9770dbffd7eddc76d02f4f3cf4211006

---
+++ src/Panel/AssocLinks.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace BlueSpice\Calumma\Panel;
+
+use Skins\Chameleon\IdRegistry;
+use BlueSpice\ExtensionAttributeBasedRegistry;
+use BlueSpice\Calumma\AssocLinksCollector;
+
+class AssocLinks extends BasePanel {
+
+	protected $bodyHtml = '';
+
+	/**
+	 *
+	 * @return \Message
+	 */
+	public function getTitleMessage() {
+		return new \Message( 'bs-sitetools-associated-links' );
+	}
+
+	/**
+	 *
+	 * @return string
+	 */
+	public function getBody() {
+		$this->initBodyHtml();
+		return $this->bodyHtml;
+	}
+
+	protected $bodyHtmlAlreadyInitialized = false;
+
+	private function initBodyHtml() {
+		if ( $this->bodyHtmlAlreadyInitialized ) {
+			return;
+		}
+
+		$registry = new ExtensionAttributeBasedRegistry(
+			'BlueSpiceFoundationAssocLinksProviderRegistry'
+		);
+
+		$this->skintemplate instanceof \BaseTemplate;
+
+		$collector = new AssocLinksCollector(
+			$registry,
+			$this->skintemplate->getSkin()->getContext(),
+			$this->skintemplate->getSkin()->getConfig()
+		);
+
+		$links = new LinkList(
+			'dummy-id',
+			[
+				'content' => $collector->getLinkDefs()
+			]
+		);
+
+		$this->bodyHtml = $links->getBody();
+		$this->bodyHtmlAlreadyInitialized = true;
+	}
+
+	/**
+	 *
+	 * @param \IContextSource $context
+	 * @return bool
+	 */
+	public function shouldRender( $context ) {
+		$this->initBodyHtml();
+		if ( empty( $this->bodyHtml ) ) {
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 *
+	 * @var string
+	 */
+	protected $htmlId = null;
+
+	/**
+	 * The HTML ID for thie component
+	 * @return string
+	 */
+	public function getHtmlId() {
+		if ( $this->htmlId === null ) {
+			$this->htmlId = IdRegistry::getRegistry()->getId( 'bs-associated-links' );
+		}
+		return $this->htmlId;
+	}
+}
