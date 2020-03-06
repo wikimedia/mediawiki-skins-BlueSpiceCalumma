e2eef75189b474eb2f4fd9a91883869e9076ebb7
pwirth <wirth@hallowelt.biz>
2020-03-06 10:19:23 +0100

    Replace BlueSpice\Services shortcuts with getService
    ERM:18635
    [3.2]
    
    Change-Id: I0de9b486998df5c3e808296b00d0407e1027ea8c

--- src/Hook/UserLoggedIn/AddNotification.php
+++ src/Hook/UserLoggedIn/AddNotification.php
@@ -7,7 +7,7 @@ use BlueSpice\SimpleDeferredNotification;
 class AddNotification extends UserLoggedIn {
 
 	protected function doProcess() {
-		$this->getServices()->getBSDeferredNotificationStack()->push(
+		$this->getServices()->getService( 'BSDeferredNotificationStack' )->push(
 			new SimpleDeferredNotification(
 				[
 					'message' => $this->msg( 'bs-calumma-notification-logged-in' ),
