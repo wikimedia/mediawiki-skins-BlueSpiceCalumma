bb2ab44d0b04a00171fe68b033ff097e0258f808
dvogel <daniel.vogel@hallowelt.com>
2020-06-12 09:59:55 +0200

    Some methods have been set to private in BaseTemplate. This is a workarround until the Skin:Chameleon components are not fixed.
    [3.2]
    
    ERM19353
    
    Change-Id: I382b2f4bcdb2835da12f5e7351e34643be27734c

--- src/Template.php
+++ src/Template.php
@@ -37,4 +37,79 @@ class Template extends \Skins\Chameleon\ChameleonTemplate {
 		$this->skinDataFieldDefinition->populateDefaultData();
 		return parent::execute();
 	}
+
+	/**
+	 * This is a cooy of BaseTemplate:getFooterLinks which was set
+	 * to private.
+	 *
+	 * We need this until the Skin:Chameleon component is updated.
+	 *
+	 * @param string|null $option
+	 * @return array|mixed
+	 */
+	public function getFooterLinks( $option = null ) {
+		$footerlinks = $this->get( 'footerlinks' );
+
+		// Reduce footer links down to only those which are being used
+		$validFooterLinks = [];
+		foreach ( $footerlinks as $category => $links ) {
+			$validFooterLinks[$category] = [];
+			foreach ( $links as $link ) {
+				if ( isset( $this->data[$link] ) && $this->data[$link] ) {
+					$validFooterLinks[$category][] = $link;
+				}
+			}
+			if ( count( $validFooterLinks[$category] ) <= 0 ) {
+				unset( $validFooterLinks[$category] );
+			}
+		}
+
+		if ( $option == 'flat' ) {
+			// fold footerlinks into a single array using a bit of trickery
+			$validFooterLinks = array_merge( ...array_values( $validFooterLinks ) );
+		}
+
+		return $validFooterLinks;
+	}
+
+	/**
+	 * This is a cooy of BaseTemplate:getFooterIcons which was set
+	 * to private.
+	 *
+	 * We need this until the Skin:Chameleon component is updated.
+	 *
+	 * @param string|null $option
+	 * @deprecated 1.35 read footer icons from template data requested via
+	 *     $this->get('footericons')
+	 * @return array
+	 */
+	public function getFooterIcons( $option = null ) {
+		// Generate additional footer icons
+		$footericons = $this->get( 'footericons' );
+
+		if ( $option == 'icononly' ) {
+			// Unset any icons which don't have an image
+			foreach ( $footericons as &$footerIconsBlock ) {
+				foreach ( $footerIconsBlock as $footerIconKey => $footerIcon ) {
+					if ( !is_string( $footerIcon ) && !isset( $footerIcon['src'] ) ) {
+						unset( $footerIconsBlock[$footerIconKey] );
+					}
+				}
+			}
+			// Redo removal of any empty blocks
+			foreach ( $footericons as $footerIconsKey => &$footerIconsBlock ) {
+				if ( count( $footerIconsBlock ) <= 0 ) {
+					unset( $footericons[$footerIconsKey] );
+				}
+			}
+		} elseif ( $option == 'nocopyright' ) {
+			unset( $footericons['copyright']['copyright'] );
+			if ( count( $footericons['copyright'] ) <= 0 ) {
+				unset( $footericons['copyright'] );
+			}
+		}
+
+		return $footericons;
+	}
+
 }
