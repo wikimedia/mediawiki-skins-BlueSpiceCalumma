1adc477798a9da84d88806e39bc401e3c20ab4b6
dvogel <daniel.vogel@hallowelt.com>
2020-08-14 14:08:56 +0200

    Use less vars to apply FlexiSkin colors
    [3.3]
    
    ERM19138
    
    Change-Id: Ica47883b64349cfae1bd032acb391f580710aea1

--- src/Setup.php
+++ src/Setup.php
@@ -2,6 +2,8 @@
 
 namespace BlueSpice\Calumma;
 
+use MWStake\MediaWiki\Component\CommonUserInterface\LessVars;
+
 class Setup {
 	/**
 	 *
@@ -21,5 +23,31 @@ class Setup {
 				"header" => "\\BlueSpice\\Calumma\\CustomMenu\\Header::getInstance"
 			]
 		];
+
+		self::setLessVars();
+	}
+
+	/**
+	 *
+	 */
+	private static function setLessVars() {
+		$lessVars = LessVars::getInstance();
+
+		$lessVars->setVar( 'content-width', '61.25rem' );
+		$lessVars->setVar( 'body-bg', '#ffffff' );
+		$lessVars->setVar( 'content-bg', '#ffffff' );
+		$lessVars->setVar( 'content-fg', '#333333' );
+		$lessVars->setVar( 'content-h1-fg', '#333333' );
+		$lessVars->setVar( 'content-h1-border', 'none' );
+		$lessVars->setVar( 'content-h2-fg', '#333333' );
+		$lessVars->setVar( 'content-h2-border', '1px solid currentColor' );
+		$lessVars->setVar( 'content-h3-fg', '#333333' );
+		$lessVars->setVar( 'content-h3-border', 'none' );
+		$lessVars->setVar( 'content-h4-fg', '#333333' );
+		$lessVars->setVar( 'content-h4-border', 'none' );
+		$lessVars->setVar( 'content-h5-fg', '#333333' );
+		$lessVars->setVar( 'content-h5-border', 'none' );
+		$lessVars->setVar( 'content-h6-fg', '#333333' );
+		$lessVars->setVar( 'content-h6-border', 'none' );
 	}
 }
